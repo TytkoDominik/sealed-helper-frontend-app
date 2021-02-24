5323ab169cf2109366810d813969518cee92df27
Tytko <dominik.d@yggdrasilgaming.com>
2021-02-24 15:18:50 +0100

    test

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://sealed-helper.azurewebsites.net/events'
+    baseURL: 'http://localhost:5000/events'
 })
 
 export const getPlayerDeck = payload => api.post(`/sealed/player`, payload)
